963e70f00b9d438eaf74fcd83750bd2c9290f56f
watisluv1910 <nasevich.valsd.03@gmail.com>
2022-11-30 03:55:57 +0300

    Added short header.

---
+++ js/header-rollup.js
@@ -0,0 +1,21 @@
+const header = document.querySelector('header');
+
+let border = convertRemToPixels(
+    getComputedStyle(document.body)
+    .getPropertyValue('--col-gap-default')
+    .slice(0, -3)
+    ) / 2;
+
+document.addEventListener('scroll', function() {
+    if (window.scrollY > border & !header.classList.contains('rolled-up')) {
+        header.classList.add('rolled-up');
+    } else if (window.scrollY <= border & header.classList.contains('rolled-up')) {
+        header.classList.remove('rolled-up');
+    }
+});
+
+function convertRemToPixels(rem) {    
+    return rem * parseFloat(
+        getComputedStyle(document.documentElement).fontSize
+        );
+};
